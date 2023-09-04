5cd85ca5e8e75f1282948baa739a35972f785eda
nithish-k <90983075+nithish-k@users.noreply.github.com>
2023-09-04 15:38:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let video;
+
+const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
+
+function setup() {
+    video = createCapture(VIDEO);
+    let w = video.width;
+    let h = video.height;
+    video.size(windowWidth-20, windowWidth*(h/w));
+    video.hide();
+    let cnv = createCanvas(windowWidth-20, windowWidth*(h/w));
+    cnv.id('mycanvas');
+}
+
+styleType = 0;
+  
+function draw() { 
+    background(255);
+    gridSize = floor(windowWidth/100);
+    video.loadPixels();
+    for (let y=0; y<video.height; y+=gridSize) {
+      for (let x=0; x<video.width; x+=gridSize) {
+        let index = (y * video.width - x) * 4;
+        let r = video.pixels[index];
+        let ind = floor(map(r, 0,255, 0, density.length));
+        let dia = floor(map(r, 0,255, gridSize, 0));
+        fill(0);
+        noStroke();
+        textSize(gridSize);
+        if(styleType == 0){
+            text(density.charAt(ind), x, y);
+        }
+        else if(styleType == 1){
+            circle(x+gridSize/2,y+gridSize/2, dia);
+        }
+        else{
+            square(x+gridSize/2,y+gridSize/2, dia);
+        }
+      }
+    }
+}
+
+function changeStyle(elem){
+    styleType = elem.value;
+    console.log(styleType);
+}
