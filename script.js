cb8e642bb2edeaca8214d9eab7f2135622965000
HauseMasterZ <113833707+HauseMasterZ@users.noreply.github.com>
2023-09-05 16:55:25 +0530

    Fix JS

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
 let video;
 let styleType = 0;
 
-const density = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,^`'. ";
+const density = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'. ";
 
-const saveToFile = () => {
+function saveToFile() {
     saveCanvas('ASCII', 'png');
-};
+}
 
-const setup = () => {
-    const video = createCapture(VIDEO);
-    const { width, height } = video;
-    video.size(windowWidth - 20, windowWidth * (height / width));
+function setup() {
+    video = createCapture(VIDEO);
+    let w = video.width;
+    let h = video.height;
+    video.size(windowWidth - 20, windowWidth * (h / w));
     video.hide();
-    const cnv = createCanvas(windowWidth - 20, windowWidth * (height / width));
+    let cnv = createCanvas(windowWidth - 20, windowWidth * (h / w));
     cnv.id('mycanvas');
-    const saveBtn = createButton('save');
-    saveBtn.position(0, windowWidth * (height / width) + 60);
+    saveBtn = createButton("save");
+    saveBtn.position(0, windowWidth * (h / w) + 60);
     saveBtn.mousePressed(saveToFile);
-    saveBtn.id('saveBtn');
-};
+    saveBtn.id("saveBtn");
+}
 
-const draw = () => {
+
+function draw() {
     background(255);
-    const gridSize = floor(windowWidth / 125);
+    gridSize = floor(windowWidth / 125);
     video.loadPixels();
     for (let y = 0; y < video.height; y += gridSize) {
         for (let x = 0; x < video.width; x += gridSize) {
-            const index = (y * video.width - x) * 4;
-            const [r, g, b] = video.pixels.slice(index, index + 3);
-            const avg = floor((r + g + b) / 3);
-            const ind = floor(map(avg, 0, 255, 0, density.length));
-            const dia = floor(map(avg, 0, 255, gridSize, 0));
+            let index = (y * video.width - x) * 4;
+            let r = video.pixels[index];
+            let g = video.pixels[index + 1];
+            let b = video.pixels[index + 2];
+            let avg = floor((r + g + b) / 3);
+            let ind = floor(map(avg, 0, 255, 0, density.length));
+            let dia = floor(map(avg, 0, 255, gridSize, 0));
             fill(0);
             noStroke();
             textSize(gridSize);
-            if (styleType === 0) {
+            if (styleType == 0) {
                 text(density.charAt(ind), x, y);
-            } else if (styleType === 1) {
+            }
+            else if (styleType == 1) {
                 circle(x + gridSize / 2, y + gridSize / 2, dia);
-            } else {
+            }
+            else {
                 square(x + gridSize / 2, y + gridSize / 2, dia);
             }
         }
     }
-};
+}
 
 function changeStyle(elem) {
     styleType = elem.value;
