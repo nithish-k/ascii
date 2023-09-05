d49cdeaec241d712a7f7927bd9ac72be7d8751c6
nithish-k <me.nithishk@gmail.com>
2023-09-05 17:04:30 +0530

    fixing bad pr

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 let video;
 let styleType = 0;
 
-const density = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,^`'. ";
+let density = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,^`'. ";
 
-const saveToFile = () => {
+function saveToFile(){
     saveCanvas('ASCII', 'png');
-};
+}
 
-const setup = () => {
-    const video = createCapture(VIDEO);
-    const { width, height } = video;
+function setup(){
+    let video = createCapture(VIDEO);
+    let { width, height } = video;
     video.size(windowWidth - 20, windowWidth * (height / width));
     video.hide();
-    const cnv = createCanvas(windowWidth - 20, windowWidth * (height / width));
+    let cnv = createCanvas(windowWidth - 20, windowWidth * (height / width));
     cnv.id('mycanvas');
-    const saveBtn = createButton('save');
+    let saveBtn = createButton('save');
     saveBtn.position(0, windowWidth * (height / width) + 60);
     saveBtn.mousePressed(saveToFile);
     saveBtn.id('saveBtn');
-};
+}
 
-const draw = () => {
+function draw(){
     background(255);
-    const gridSize = floor(windowWidth / 125);
+    let gridSize = floor(windowWidth / 125);
     video.loadPixels();
     for (let y = 0; y < video.height; y += gridSize) {
         for (let x = 0; x < video.width; x += gridSize) {
-            const index = (y * video.width - x) * 4;
-            const [r, g, b] = video.pixels.slice(index, index + 3);
-            const avg = floor((r + g + b) / 3);
-            const ind = floor(map(avg, 0, 255, 0, density.length));
-            const dia = floor(map(avg, 0, 255, gridSize, 0));
+            let index = (y * video.width - x) * 4;
+            let [r, g, b] = video.pixels.slice(index, index + 3);
+            let avg = floor((r + g + b) / 3);
+            let ind = floor(map(avg, 0, 255, 0, density.length));
+            let dia = floor(map(avg, 0, 255, gridSize, 0));
             fill(0);
             noStroke();
             textSize(gridSize);
@@ -43,7 +43,7 @@ const draw = () => {
             }
         }
     }
-};
+}
 
 function changeStyle(elem) {
     styleType = elem.value;
